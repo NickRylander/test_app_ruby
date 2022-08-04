57e82fbc2184bea1d47064a3c5eb6ac52a1a1d65
Nicholas Rylander <nick.rylander@outlook.com>
2022-08-04 16:26:19 -0700

    authenticator

--- playground.rb
+++ playground.rb
@@ -7,3 +7,36 @@
 # IF ELSE
 
 
+# LOOPS
+
+# a = (1..10).to_a
+
+# for items in a
+#   puts items
+# end
+
+# a.each do |num|
+#   puts num
+# end
+
+# a.each {|num| puts num}
+
+# a.select {|num| num.odd?}
+
+# HASH
+
+=begin simple_hash = {'a' => 1, 'b' => 2, 'c' => 3}
+p simple_hash.keys
+p simple_hash.values
+
+simple_hash.each do |key, value|
+  puts "The value of #{key} key is #{value}"
+end
+
+another_hash = {a: 1, b: 2, c: 3, d: 4}
+p another_hash[:a]
+another_hash[:e] = 5
+p another_hash
+
+another_hash.each {|key, value| puts "The key is #{key} and the value is #{value}"}
+another_hash.select {|k, v| puts v.odd?} =end

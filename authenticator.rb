8a45c270447c1a579d0745a13ce8ac8eeda4dda9
Nicholas Rylander <nick.rylander@outlook.com>
2022-08-05 08:30:54 -0700

    finished authenticator

--- authenticator.rb
+++ authenticator.rb
@@ -13,8 +13,27 @@ def title_card
   puts "This program will take input from the user and compare the password"
 end
 
+def auth_user(username, password, list_of_users)
+  list_of_users.each do |user|
+    if user[:username] == username && user[:password] == password
+      return user
+    end
+  end
+  return "Credentials are invalid"
+end
+
 title_card()
-print "Username: "
-user = gets.chomp
-print "Password: "
-pass = gets.chomp 
+attempts = 1
+while attempts < 4
+  print "Username: "
+  username = gets.chomp
+  print "Password: "
+  password = gets.chomp 
+  authentication = auth_user(username, password, users)
+  puts authentication
+  puts "Press n to quit or any other key to continue: "
+  input = gets.chomp.downcase
+  break if input == "n"
+  attempts += 1
+end
+puts "You have exceeded the number of attempts" if attempts == 4

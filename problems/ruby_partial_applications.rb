d0f88dcb39b3162433fe547316b3b042f3f71c09
Nicholas Rylander <nick.rylander@outlook.com>
2022-08-09 09:05:50 -0700

    ruby partial applications solutions 1

---
+++ problems/ruby_partial_applications.rb
@@ -0,0 +1,12 @@
+# Here, combination is a variable that stores a partial application which computes combination nCr.
+
+combination = -> (n) do
+  -> (r) do
+    (n-r+1..n).inject(:*) / (1..r).inject(:*)
+  end
+end
+
+n = gets.to_i
+r = gets.to_i
+nCr = combination.(n)
+puts nCr.(r)

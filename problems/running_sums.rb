c07de234200fec3c7e6d63946d96d4ab156dd2b9
Nicholas Rylander <nick.rylander@outlook.com>
2022-08-14 13:38:50 -0700

    start leet running sums

--- problems/running_sums.rb
+++ problems/running_sums.rb
@@ -2,7 +2,11 @@
 # Given the head of a singly linked list, return true if it is a palindrome.
 
 def is_palindrome(head)
-    p head
+    if head.reverse === head
+      p true
+    else
+      p false
+    end
 end
 
 is_palindrome([1, 2, 2, 1])

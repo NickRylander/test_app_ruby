852a0d68a328eedb40cf55f8fc46afbb51a25ac0
Nicholas Rylander <nick.rylander@outlook.com>
2022-08-14 13:16:52 -0700

    start leet paindrome

---
+++ problems/leet_palindrome_link_list.rb
@@ -0,0 +1,10 @@
+# CHALLENGE
+# Given the head of a singly linked list, return true if it is a palindrome.
+
+def is_palindrome(head)
+    p head
+end
+
+is_palindrome([1, 2, 2, 1])
+is_palindrome([1, 2])
+is_palindrome([1, 2, 3, 2, 1])

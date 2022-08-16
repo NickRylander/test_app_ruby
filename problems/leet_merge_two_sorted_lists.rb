add4a496197a6cc5a8c1e609513b93d2a7888754
Nicholas Rylander <nick.rylander@outlook.com>
2022-08-16 08:31:44 -0700

    leet merge two sorted lists solution 1

--- problems/leet_merge_two_sorted_lists.rb
+++ problems/leet_merge_two_sorted_lists.rb
@@ -5,7 +5,8 @@
 # Return the head of the merged linked list.
 
 def merge_two_lists(list1, list2)
-    
+    list2.each {|num| list1 << num}
+    p list1.sort!
 end
 
 merge_two_lists([1, 2, 4], [1, 3, 4])

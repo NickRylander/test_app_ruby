2a3a110314532b5505dd393d8dcc89c9a780ce3b
Nicholas Rylander <nick.rylander@outlook.com>
2022-08-16 08:22:14 -0700

    leet valid parenthesis solution 3

--- problems/leet_valid_parentheses.rb
+++ problems/leet_valid_parentheses.rb
@@ -5,10 +5,47 @@
 # Open brackets must be closed by the same type of brackets.
 # Open brackets must be closed in the correct order.
 
+# def is_valid(s)
+#   if s.include?("()" || "[]" || "{}")
+#     p true
+#   else
+#     p false
+#   end
+# end
+
+# def is_valid(s)
+#   check = []
+#   s.chars.each do |a|
+#     check << a
+#     p check.join("")
+#     if check.join("").include?("()" || "[]" || "{}")
+#       p true
+#     else
+#       p false
+#     end
+#   end
+# end
+
 def is_valid(s)
+  return true if s.empty?
   
+  stack = []
+  s.each_char do |c|
+      case c
+      when '(', '{', '['
+          stack.push(c)
+      when ')'
+          return false if stack.pop() != '('
+      when '}'
+          return false if stack.pop() != '{'
+      when ']'
+          return false if stack.pop() != '['
+      end
+  end
+  return stack.empty?
 end
 
-is_valid("()") # => true
+# is_valid("()") # => true
 # is_valid("()[]{}") # => true
 # is_valid("(]") # => false
+is_valid("{[]}") # => true
